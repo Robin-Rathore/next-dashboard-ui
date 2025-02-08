7ce4eb5a9cd23b09f940e51cfa5c7e4c8191b33a
Robin Rathore <robinsingh248142@gmail.com>
2025-02-08 23:29:29 +0530

    All set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
+  title: "Sarvatra.ai Automatic University Software",
   description: "Next.js School Management System",
 };
 
